1118c73be3e80b1fc17abf563651ba575c24e3cb
Kairu-Astray <116314324+Kairu-Astray@users.noreply.github.com>
2023-01-03 22:18:11 +0000

    Added Prestige. Fixed Fish

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-let money = 0
-let fish = 1
-let prestige = 0
-var updateValue = 1
+let money = 0;
+let fish = 1;
+let prestige = 0;
+var updateValue = 1;
 
 function displayMoney() {
-nfObject = new Intl.NumberFormat('en-GB');
-output = nfObject.format(money);
-document.getElementById("currentMoney").innerHTML = "Money: " + output
+  nfObject = new Intl.NumberFormat("en-GB");
+  output = nfObject.format(money);
+  document.getElementById("currentMoney").innerHTML = "Money: " + output;
 }
 
 function displayFish() {
-  nfObject = new Intl.NumberFormat('en-GB');
+  nfObject = new Intl.NumberFormat("en-GB");
   output = nfObject.format(fish);
-  document.getElementById("currentFish").innerHTML = "Fish: " + output
+  document.getElementById("currentFish").innerHTML = "Fish: " + output;
 }
 
 function displayPrestige() {
-  nfObject = new Intl.NumberFormat('en-GB');
+  nfObject = new Intl.NumberFormat("en-GB");
   output = nfObject.format(prestige);
-  document.getElementById("currentOrestige").innerHTML = "Prestige: " + output
+  document.getElementById("currentOrestige").innerHTML = "Prestige: " + output;
 }
 
 async function addMoney() {
-  money += updateValue
-  displayMoney()
-  console.log(money)
+  money += updateValue;
+  displayMoney();
+  console.log(money);
 }
 
 // async function addFish() {     --- DEV command
@@ -34,38 +34,39 @@ async function addMoney() {
 // }
 
 async function printMoney() {
-  console.log(money)
+  console.log(money);
 }
 
 function buyFish() {
   if (fish >= 100) {
-    console.log("Failed, Too much fish!")
+    console.log("Failed, Too much fish!");
   } else if (money >= 10) {
-    money -= 10
-    fish += 1
-    console.log("Bought Fish!")
-    updateValue += 1
-    displayMoney()
-    displayFish()
+    money -= 10;
+    fish += 1;
+    console.log("Bought Fish!");
+    updateValue += 1;
+    displayMoney();
+    displayFish();
   }
-  displayMoney()
-  displayFish()
+  displayMoney();
+  displayFish();
 }
 
 function buyPrestige() {
   if (fish < 100 || money < 10000) {
-    console.log("Failed, You don't meet the requirements. (100 Fish, 10,000 Money)")
+    console.log(
+      "Failed, You don't meet the requirements. (100 Fish, 10,000 Money)"
+    );
   } else if (fish >= 100 || money >= 10000) {
-    money = 0
-    fish = 0
-    prestige += 1
-    console.log("Prestiged!")
-    updateValue = 1
-    displayMoney()
-    displayFish()
-    displayPrestige()
+    money = 0;
+    fish = 0;
+    prestige += 1;
+    console.log("Prestiged!");
+    updateValue = 1;
+    displayMoney();
+    displayFish();
+    displayPrestige();
   }
-  displayMoney()
-  displayFish()
+  displayMoney();
+  displayFish();
 }
-
