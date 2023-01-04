5acbb621daf66521a0534900cc332d73bdf1ddf4
Kairu-Astray <gaffneyrudy542@gmail.com>
2023-01-04 20:23:50 +0000

    modified:   index.js

--- index.js
+++ index.js
@@ -47,17 +47,21 @@ async function printMoney() {
 function buyFish() {
   if (fish >= 100) {
     alert("Failed, Too much fish!");
-  } else if (money >= 10) {
+  } else if (money >= 10 && updateValue < 2) {
     money -= 10;
     fish += 1;
     updateValue += 1;
+  } else if (money >= 10 || updateValue == 2) {
+    money -= 10;
+    fish += 1;
+    updateValue += 2;
   }
 }
 
 function buyPrestige() {
   if (fish < 100 || money < 10000) {
-    alert("Failed, You don't meet the requirements. (100 Fish, 10,000 Money)");
-  } else if (fish >= 100 || money >= 10000) {
+    alert("Failed, You don't meet the requirements. (100 Fish, 100,000 Money)");
+  } else if (fish >= 100 || money >= 100_000) {
     money = 0;
     fish = 0;
     prestige += 1;
@@ -79,5 +83,5 @@ function buyMoneyPS() {
   }
   setInterval(function coinPS() {
     money += moneyPS;
-  }, 1000);
+  }, 1_000);
 }
